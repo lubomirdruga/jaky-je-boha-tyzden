d5f64e2a860adc091538aa8c47a9a3545b2367d7
Ľubomír Druga <drugalubomir@gmail.com>
2020-11-26 16:20:51 +0100

    set dates to midnight, removes rounding bug

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 
 const semesterStart = new Date('2020-09-21');
-const now = new Date();
+semesterStart.setHours(0,0,0,0);
+let now = new Date();
+now.setHours(0,0,0,0);
 
 function week() {
-    now.getDay()
     let currentWeek = Math.round((now - semesterStart) / (7 * 24 * 60 * 60 * 1000));
     currentWeek = [0, 5, 6].includes(now.getDay()) ? currentWeek : currentWeek + 1;
     const element = document.getElementById("week");
